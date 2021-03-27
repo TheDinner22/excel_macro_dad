eb46d843a0b463708b3cd9c451cfb4ff59739a34
Joseph Goodman <jgood1133@gmail.com>
2021-03-27 15:09:55 -0400

    quick change so a useless test is not running

--- test/integration.py
+++ test/integration.py
@@ -2,9 +2,14 @@
 
 integration_tests = {}
 
-def one_plus_one_is_two(done):
+
+
+
+# example test below
+
+"""def one_plus_one_is_two(done):
     outcome = 1+1
     desired_outcome = 2
     assert outcome == desired_outcome, "1+1 was not equal to two"
     done("one plus one is equal to two")
-integration_tests["one plus one is equal to two"] = one_plus_one_is_two
+integration_tests["one plus one is equal to two"] = one_plus_one_is_two"""
